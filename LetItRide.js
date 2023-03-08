408c948997c64a59f109c55753068ee80415db27
Karan Patel <karanpatel4@cmail.carleton.ca>
2023-03-07 20:46:30 -0500

    generate hand

--- LetItRide.js
+++ LetItRide.js
@@ -1,3 +1,5 @@
+// Let It Ride Poker Game
+
 // constants for creating the deck
 const suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
 const values = [
@@ -36,14 +38,15 @@ const cards = () => {
   return cards;
 };
 
-// create a deck
-let deck = cards();
-
-// print out the deck
-const printDeck = (deck) => {
+// shuffle the deck
+const shuffle = (deck) => {
   for (let i = 0; i < deck.length; i++) {
-    console.log(deck[i].value + " of " + deck[i].suit);
+    const randomIndex = Math.floor(Math.random() * deck.length);
+    const temp = deck[i];
+    deck[i] = deck[randomIndex];
+    deck[randomIndex] = temp;
   }
+  return deck;
 };
 
 // get a random card from the deck and remove it from the deck
@@ -53,7 +56,50 @@ const getRandomCard = (deck) => {
   return randomCard;
 };
 
-// reset the deck
-const resetDeck = () => {
-  deck = cards();
+// deal a hand of 3 cards
+const deal3Hand = () => {
+  for (let i = 0; i < 3; i++) {
+    hand.push(getRandomCard(deck));
+  }
+  return hand;
 };
+
+// deal 4th card
+const deal4thCard = () => {
+  tempCard = getRandomCard(deck);
+  hand.push(tempCard);
+  return tempCard;
+};
+
+// deal 5th card
+const deal5thCard = () => {
+  tempCard = getRandomCard(deck);
+  hand.push(tempCard);
+  return tempCard;
+};
+
+// print out the deck
+const printDeck = (deck) => {
+  for (let i = 0; i < deck.length; i++) {
+    console.log(deck[i].value + " of " + deck[i].suit);
+  }
+};
+
+// print out the hand
+const printHand = (hand) => {
+  for (let i = 0; i < hand.length; i++) {
+    console.log(hand[i].value + " of " + hand[i].suit);
+  }
+};
+
+// create a deck
+let deck = shuffle(cards());
+const hand = [];
+
+console.log("Let It Ride Poker Game");
+console.log("------ 3 Card Hand ------");
+printHand(deal3Hand());
+console.log("------ 4th Card ------");
+printHand([deal4thCard()]);
+console.log("------ 5th Card ------");
+printHand([deal5thCard()]);
